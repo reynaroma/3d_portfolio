fd12808ac951d4cef5aa13a0df4bc3ce2964d6b4
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-15 17:39:36 -0600

    Add and style a header and subheader texts on Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,10 +9,20 @@ const Hero = () => {
       <div className={`${styles.paddingX} absolute inset-0
       top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='h-5 w-5 rounded-full bg-[#915eff]'/>
-          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+          <div className='h-5 w-5 rounded-full bg-[#915eff]' />
+          <div className='w-1 sm:h-80 h-60 violet-gradient' />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} 
+          text-white`}>Hi, I&lsquo;m <span
+              className='text-[#915eff]'>Reyna</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop 3D visuals, user <br className='sm:block hidden' /> interfaces and web applications
+          </p>
         </div>
       </div>
+      <ComputersCanvas />
     </section>
   )
 }
