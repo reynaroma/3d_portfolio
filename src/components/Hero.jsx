b1fac5cbb41a6a4775de52116a6960dffa9de9ac
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-15 16:54:46 -0600

    Add vertical line enxt to circle

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,6 +10,7 @@ const Hero = () => {
       top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='h-5 w-5 rounded-full bg-[#915eff]'/>
+          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
         </div>
       </div>
     </section>
