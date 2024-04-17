2a75247268f8deb6a57708820551f014621e176e
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-17 16:14:59 -0600

    Add imports on the Exp component and wrap it with SectionWrapper

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,24 @@
 import { VerticalTimeline, VerticalTimelineElement } from
-  "react-vertical-timeline-component"
+  "react-vertical-timeline-component";
+
+import { motion } from "framer-motion";
+import 'react-vertical-timeline-component/style.min.css';
+
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
+import { styles } from "../styles";
+
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Work Experience</h2>
+      </motion.div>
+    </>
   )
 }
 
-export default Experience
+export default SectionWrapper(Experience, 'experience')
