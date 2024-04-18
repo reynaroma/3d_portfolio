49baef9fc50747558224a3783b6a20a5eba8453d
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-17 18:31:11 -0600

    Add effects and animations on the 3D model

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,7 +2,6 @@ import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
-
 const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap 
