350254490212bf7cdfa3c1b03e2b6034342a9201
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-18 15:45:53 -0600

    Create a ProjectCard component and add styles on the test output

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Tilt } from 'react-tilt';
-import { motion } from 'framer-motion';
+import { motion, spring } from 'framer-motion';
 
 import { styles } from '../styles';
 import { github } from '../assets';
 import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
+
+const ProjectCard = ({ index, name, description,
+  tags, image, source_code_link }) => {
+  return (
+    <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)}>
+      test
+    </motion.div>
+  )
+}
 
 const Works = () => {
   return (
@@ -30,6 +39,16 @@ const Works = () => {
           repositories and live demos in it. It reflects my ability to solve problems,
           work with different technologies, and manage projects effectively.
         </motion.p>
+      </div>
+
+      <div className='mt-20 flex flex-wrap gap-7'>
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={`project-${index}`}
+            index={index}
+            {...project}
+          />
+        ))}
       </div>
     </>
   )
