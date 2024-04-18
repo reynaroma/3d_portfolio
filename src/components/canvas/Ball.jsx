908f350e22b01e7402d09519e7f527656b6d4b18
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-17 19:00:10 -0600

    Add extra animation on floating elements

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -6,19 +6,22 @@ import React, { Suspense } from 'react';
 // OrbitControls is a component that allows us to move the camera around the scene
 // Preload is a component that allows us to preload assets
 // Canvas is a component that allows us to create a 3D scene
-import { Canvas } from '@react-three/fiber';
+import { Canvas } from "@react-three/fiber";
 import {
-  Decal, Float, OrbitControls, Preload, useTexture
-} from '@react-three/drei';
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
 
-import CanvasLoader from '../Loader';
+import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={1.75} rotationIntensity={1}
-      floatIntensity={2}>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
@@ -32,17 +35,20 @@ const Ball = (props) => {
         <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
           map={decal}
+          flatShading
         />
       </mesh>
     </Float>
-  )
-}
+  );
+};
 
 const BallCanvas = ({ icon }) => {
   return (
     <Canvas
-      frameloop="demand"
+      frameloop='demand'
+      dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
@@ -52,7 +58,7 @@ const BallCanvas = ({ icon }) => {
 
       <Preload all />
     </Canvas>
-  )
-}
+  );
+};
 
 export default BallCanvas;
