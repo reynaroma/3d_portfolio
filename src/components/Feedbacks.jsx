a7a4b8e12470bcde516372b78f2590cf8d7109b8
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-18 17:39:14 -0600

    Add style on the designation and company

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -22,7 +22,7 @@ const FeedbackCard = ({ index, testimonial, name,
           <p className="text-white font-medium text-[16px]">
             <span className="blue-text-gradient">@</span> {name}
           </p>
-          <p>
+          <p className="mt-1 text-secondary text-[12px]">
             {designation} of {company}
           </p>
         </div>
