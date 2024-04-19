0ba0d79754fb600482d80eb8289947fe6f9ac154
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-18 19:03:12 -0600

    Add library to import and to load the EarthCanvas

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import { Suspense } from "react";
+// anything 3D related should be wrapped in a Suspense component
+// to catch any errors that might occur during rendering
+// and display a fallback UI
+
+// anything related to 3D appears on a canvas
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei"; // helper library
+import CanvasLoader from '../Loader';
 
 const Earth = () => {
   return (
@@ -6,4 +14,14 @@ const Earth = () => {
   )
 }
 
-export default Earth
+const EarthCanvas =() => {
+  return (
+    <Canvas
+    
+    >
+
+    </Canvas>
+  )
+}
+
+export default EarthCanvas;
