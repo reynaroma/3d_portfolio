6c9081358bdce0aeac8263e6bba8c3775e204948
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-18 18:52:13 -0600

    Add send button and style it

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -84,6 +84,14 @@ const Contact = () => {
               rounded-lg outline-none border-none font-medium"
             />
           </label>
+
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit
+            text-white font-bold shadow-md shadow-primary rounded-xl"
+          >
+            {loading ? 'Sending...' : 'Send'}
+          </button>
         </form>
       </motion.div>
     </div>
