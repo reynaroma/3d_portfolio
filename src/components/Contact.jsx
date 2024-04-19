8ee7c8eb4a44fe57551cfc0978889c30151718c8
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-18 18:12:16 -0600

    Create empty handlers and style the wrapper for the contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,8 +8,31 @@ import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
 const Contact = () => {
+  // form ref
+  const formRef = useRef();
+  // form state
+  const [form, setform] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  // loading when sending email
+  const [loading, setloading] = useState(false);
+
+  // handle form input change
+  const handleChange = (e) => {
+
+  }
+  // handle form submission 
+  const handleSubmit = (e) => {
+
+  }
+
   return (
-    <div>Contact</div>
+    <div className="xl:mt-12 xl:flex-row
+    flex-col-reverse flex gap-10 overflow-hidden ">
+      Contact
+    </div>
   )
 }
 
