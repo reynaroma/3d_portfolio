a64b6a664ba275809a63bc946ac4831557a0a460
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-18 18:40:27 -0600

    Add attributes on the contact input

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,7 +31,30 @@ const Contact = () => {
   return (
     <div className="xl:mt-12 xl:flex-row
     flex-col-reverse flex gap-10 overflow-hidden ">
-      Contact
+      <motion.div
+        variants={slideIn('left', 'tween', 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8
+        rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white 
+            font-medium mb-4">Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+            />
+          </label>
+        </form>
+      </motion.div>
     </div>
   )
 }
