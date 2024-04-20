0dd81d122481541e536efd4eab431296e8b9d248
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-19 18:49:09 -0600

    Remove the positions of the stars cause its giving me some errors

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,50 +1,43 @@
-import { useRef, Suspense } from 'react';
+/* eslint-disable react/no-unknown-property */
+import { useState, useRef, Suspense } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Points, PointMaterial, Preload } from '@react-three/drei';
 import * as random from 'maath/random/dist/maath-random.esm';
 
 const Stars = (props) => {
+  // create a ref to the Points component
   const ref = useRef();
-  
-  // Generate positions for stars without NaN values
-  const generatePositions = () => {
-    const positions = random.inSphere(new Float32Array(1000), { radius: 1.2 });
-    // Ensure that positions array does not contain NaN values
-    for (let i = 0; i < positions.length; i++) {
-      if (isNaN(positions[i])) {
-        positions[i] = 0; // Replace NaN with 0
-      }
-    }
-    return positions;
-  };
+  // create sphere of stars
+  const sphere = random.inSphere(new Float32Array(1000), { radius: 1.2 });
 
-  const positions = generatePositions();
-
+  // animate the stars to rotate them
+  // useFrame is a hook that runs every frame
   useFrame((state, delta) => {
+    // rotate the stars
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 15;
-  });
+  })
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
       <Points
-        ref={ref}
-        positions={positions} // Use generated positions without NaN values
-        stride={3}
-        frustumCulled
-        {...props}
+        ref={ref} // assign the ref to the Points component
+        // positions={sphere} // positions of the stars
+        stride={3} // 3 values per point
+        frustumCulled // don't draw if the stars are not in the view
+        {...props} // pass the props to the Points component
       >
         <PointMaterial
           transparent
-          color="#f272c8"
-          size={0.002}
-          sizeAttenuation={true}
+          color="#f272c8" // color of the stars
+          size={0.002} // size of the stars
+          sizeAttenuation={true} // make the stars smaller as they move away
           depthWrite={false}
         />
       </Points>
     </group>
-  );
-};
+  )
+}
 
 const StarsCanvas = () => {
   return (
@@ -58,7 +51,7 @@ const StarsCanvas = () => {
         <Preload all />
       </Canvas>
     </div>
-  );
-};
+  )
+}
 
 export default StarsCanvas;
